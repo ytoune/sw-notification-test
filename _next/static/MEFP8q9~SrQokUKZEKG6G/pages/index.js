2f0d0b48722c85d301780b9a0bee9790a80e99d1
toune <ytoune@users.noreply.github.com>
2018-12-06 23:01:41 +0900

    Updates

---
+++ _next/static/MEFP8q9~SrQokUKZEKG6G/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{203:function(n,e,t){__NEXT_REGISTER_PAGE("/",function(){return n.exports=t(204),{page:n.exports.default}})},204:function(n,e,t){"use strict";t.r(e);var o=t(10),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"home")}}},[[203,1,0]]]);
--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
 self.__precacheManifest = [
   {
-    "url": "https://ytoune.github.io/sw-notification-test/_next/static\\nNeuZylsf~9mCJH~sVsJL\\pages\\index.js"
+    "url": "/sw-notification-test/_next/static\\MEFP8q9~SrQokUKZEKG6G\\pages\\index.js"
   },
   {
-    "url": "https://ytoune.github.io/sw-notification-test/_next/static\\nNeuZylsf~9mCJH~sVsJL\\pages\\_error.js"
+    "url": "/sw-notification-test/_next/static\\MEFP8q9~SrQokUKZEKG6G\\pages\\_error.js"
   },
   {
-    "url": "https://ytoune.github.io/sw-notification-test/_next/static\\nNeuZylsf~9mCJH~sVsJL\\pages\\_app.js"
+    "url": "/sw-notification-test/_next/static\\MEFP8q9~SrQokUKZEKG6G\\pages\\_app.js"
   },
   {
-    "url": "https://ytoune.github.io/sw-notification-test/_next/static/runtime/webpack-42652fa8b82c329c0559.js"
+    "url": "/sw-notification-test/_next/static/runtime/webpack-42652fa8b82c329c0559.js"
   },
   {
-    "url": "https://ytoune.github.io/sw-notification-test/_next/static/runtime/main-accd5aba59124da142fb.js"
+    "url": "/sw-notification-test/_next/static/runtime/main-83433c42adbeadba562d.js"
   },
   {
-    "url": "https://ytoune.github.io/sw-notification-test/_next/static/chunks/commons.1b1a347c198e888a7b36.js"
+    "url": "/sw-notification-test/_next/static/chunks/commons.0ee2b9c6baca7daa1c80.js"
   }
 ];
 
